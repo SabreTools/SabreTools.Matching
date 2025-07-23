6f6d071a791c15b2d4d1eb312b707c71d41a5b85
Matt Nadareski <mnadareski@outlook.com>
2025-07-23 09:58:46 -0400

    Add summaries to IMatch

--- SabreTools.Matching/IMatch.cs
+++ SabreTools.Matching/IMatch.cs
@@ -1,7 +1,13 @@
 ﻿namespace SabreTools.Matching
 {
+    /// <summary>
+    /// Represents a matcher for a particular type
+    /// </summary>
     public interface IMatch<T>
     {
+        /// <summary>
+        /// Nullable typed data to be matched
+        /// </summary>
         T? Needle { get; }
     }
 }
