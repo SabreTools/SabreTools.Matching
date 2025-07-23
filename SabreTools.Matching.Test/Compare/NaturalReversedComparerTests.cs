0c9e255d48581212a54cd751231306180884232c
Matt Nadareski <mnadareski@outlook.com>
2025-07-23 10:08:30 -0400

    Split tests to match classes

--- SabreTools.Matching.Test/Compare/NaturalReversedComparerTests.cs
+++ SabreTools.Matching.Test/Compare/NaturalReversedComparerTests.cs
@@ -5,31 +5,31 @@ using Xunit;
 
 namespace SabreTools.Matching.Test.Compare
 {
-    public class NaturalComparerTests
+    public class NaturalReversedComparerTests
     {
         [Fact]
-        public void NaturalComparer_ListSort_Numeric()
+        public void NaturalReversedComparer_ListSort_Numeric()
         {
             // Setup arrays
             string[] sortable = ["0", "100", "5", "2", "1000"];
-            string[] expected = ["0", "2", "5", "100", "1000"];
+            string[] expected = ["1000", "100", "5", "2", "0"];
 
             // Run sorting on array
-            Array.Sort(sortable, new NaturalComparer());
+            Array.Sort(sortable, new NaturalReversedComparer());
 
             // Check the output
             Assert.True(sortable.SequenceEqual(expected));
         }
 
         [Fact]
-        public void NaturalComparer_ListSort_Mixed()
+        public void NaturalReversedComparer_ListSort_Mixed()
         {
             // Setup arrays
             string[] sortable = ["b3b", "c", "b", "a", "a1"];
-            string[] expected = ["a", "a1", "b", "b3b", "c"];
+            string[] expected = ["c", "b3b", "b", "a1", "a"];
 
             // Run sorting on array
-            Array.Sort(sortable, new NaturalComparer());
+            Array.Sort(sortable, new NaturalReversedComparer());
 
             // Check the output
             Assert.True(sortable.SequenceEqual(expected));
